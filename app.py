468041334df777a550b6b94f9d156141f9a4144c
Felipe0899 <felipe0308.fs@usp.br>
2025-05-03 14:44:36 -0300

    add presentation

--- app.py
+++ app.py
@@ -6,24 +6,18 @@ from google import genai
 import psycopg2
 import folium
 from streamlit.components.v1 import html
+import sqlite3
+import requests
+import geopandas
 
-API_KEY = open("gemini_api_key.txt").read()
+API_KEY = open(r"C:\Users\felip\Programing\Projeto_Certificacao\gemini_api_key.txt").read()
 
-def get_data_from_db(query):
-    conn = psycopg2.connect( 
-        host="localhost",
-        database="news_api",
-        user="postgres",
-        password="2025",
-    )
-
-    cursor = conn.cursor()
-    cursor.execute(query)
-    df = cursor.fetchall()
-
-    columns = [desc[0] for desc in cursor.description]
-    df = pd.DataFrame(df, columns=columns).set_index("id")
-    cursor.close()
+@st.cache_data
+# Function to import the database from SQLite
+def import_database(db_path):
+    conn = sqlite3.connect(db_path)
+    query = "SELECT * FROM noticias"
+    df = pd.read_sql_query(query, conn).set_index('id')
     conn.close()
     return df
 
@@ -36,88 +30,46 @@ Nosso projeto consiste em analisar notícias relacionadas às tarifas aplicadas 
 Neste projeto, nós buscamos implementar conceitos que se relacionem com as certificações que estamos estudando,
 envolvendo coleta de dados brutos por scraping, engenharia de dados com SQL e o uso de Inteligência Artificial via a API do Gemini.
 """)
-
-st.markdown("""
-## Etapas do projeto
-
-### Web Scraping
-Na etapa de Web Scraping, buscamos coletar dados de notícias relacionadas às tarifas aplicadas pelo presidente Donald Trump em alguns jornais selecionados.
-Aqui, tivemos a oportunidade de aplicar conceitos de coleta de dados brutos utilizando Python e algumas bibliotecas como BeautifulSoup, Requests, Scrapy, entre outras.
-Para alimentar o modelo do Gemini, buscamos coletar as principais informações de uma notícia de forma que a IA pudesse ter informações úteis para fazer uma análise dos dados.
-Os dados que coletamos foram:
-1. Título da notícia
-2. URL da notícia
-3. Data de publicação
-4. Conteúdo da notícia
-
-### SQL
-Para armazenar os dados coletados, utilizamos a biblioteca Sqlite3 para criarmos um database utilizando SQL.
-Essa escolha se deu pela praticidade e leveza que o SQLite oferece, permitindo a criação de um banco de dados relacional local, de fácil integração com as demais etapas do projeto, sem a necessidade de configuração de servidores externos. 
-
-Assim, garantimos maior agilidade no armazenamento, consulta e atualização das informações de maneira eficiente e organizada.
-
-### Inteligência Artificial
-Na etapa de inteligência artificial do projeto, incorporamos o uso da API do Gemini para realizar a análise avançada das notícias coletadas. 
-Através dessa integração, buscamos extrair insights qualificados sobre os impactos econômicos gerados pelos eventos noticiados, tanto em âmbito global quanto em análises específicas por país. 
-Além disso, a aplicação da IA possibilitou a avaliação da repercussão das notícias em diferentes veículos de comunicação, permitindo a identificação de padrões editoriais, variações na narrativa e possíveis vieses na cobertura jornalística. 
-Com isso, foi possível comparar o tratamento dado aos mesmos fatos por distintos jornais, enriquecendo a interpretação dos dados e oferecendo uma perspectiva crítica sobre a formação de opinião pública e seus efeitos econômicos.
-
-Ademais, utilizamos a API do Gemini para realizar uma análise de sentimentos, atribuindo scores que variam de -1 a 1, onde -1 representa uma percepção negativa, 0 uma percepção neutra e 1 uma percepção positiva.
-Essa análise nos permitiu compreender como as notícias foram recebidas e interpretadas, tanto em termos de impacto econômico quanto de percepção pública, fornecendo uma visão abrangente sobre a reação do mercado e da sociedade em relação às tarifas impostas.
-
-### Folium
-Para a visualização geográfica dos dados, utilizamos a biblioteca folium, que nos permitiu criar um mapa-múndi interativo e intuitivo.
-No mapa, cada país foi colorido de acordo com as tarifas aplicadas, adotando uma escala de intensidade: quanto maior a tarifa incidente, mais forte era a tonalidade atribuída ao território correspondente. 
-
-Essa abordagem visual facilitou a compreensão imediata dos impactos econômicos globais, destacando de forma clara as regiões mais afetadas pelas medidas tarifárias e permitindo uma análise espacial precisa dos dados coletados.
-
-### Streamlit
-Para tornar o projeto acessível e interativo, desenvolvemos uma interface utilizando o streamlit. Através dessa plataforma, o usuário pode solicitar que a inteligência artificial realize todas as análises descritas anteriormente, como a avaliação dos impactos econômicos por país, a comparação de repercussões entre jornais e a identificação de possíveis vieses na cobertura das notícias. Além disso, incorporamos ao aplicativo o mapa construído com o folium, permitindo que o usuário visualize de maneira dinâmica as tarifas aplicadas em cada região do mundo. 
-
-Complementarmente, criamos uma seção dedicada para esclarecimento de dúvidas, em que o usuário pode consultar a IA sobre aspectos específicos do projeto, como o funcionamento das bibliotecas empregadas, os aplicativos utilizados ou o próprio processo de análise de dados. Dessa forma, o projeto oferece uma experiência completa, intuitiva e educativa, reunindo análise de dados, inteligência artificial, visualização geográfica e suporte interativo em um único ambiente integrado.
-"""
-)
 # Insert the head of the dataframe here
 st.subheader("Dados utilizados")
 st.write("O dataframe abaixo contém as notícias que serão utilizadas. Elas foram geradas via Scraping e estão armazenadas em um banco de dados SQL.")
 
-query = """
-SELECT * FROM news
-"""
-df = get_data_from_db(query)
-
+df = import_database("noticias_database.db")
 st.dataframe(df.head(5))
 
 def generate_response(prompt):
-    client = genai.Client(api_key=API_KEY)
+    client = genai.Client(api_key='AIzaSyCK01vW2n-DzBs_ajmXC78BevtyhzFW9UM')
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=prompt,
     )
     return response.text
 
+# ANÁLISE GERAL ==================================================================================================
+st.subheader("Análise geral")
+st.write("Clique no botão abaixo para gerar uma análise geral sobre o impacto das tarifas no comércio internacional e na economia global.")
+
 prompt_general = f"""
 Você é um economista e está analisando notícias relacionadas as tarifas aplicadas pelo presidente Donald Trump no dia chamado por ele de "Liberation Day".
 Forneça uma breve descrição do que são essas tarifas.
 Por favor, responda em português.
 Foque em aspectos gerais da situação, sem se preocupar com o impacto em um país específico.
-Destauqe a relevância do evento e como ele pode afetar o comércio internacional. Acrescente a relevância na história econômica global e como ela se relaciona com políticas econômicas anteriores.
+Destaque a relevância do evento e como ele pode afetar o comércio internacional. Acrescente a relevância na história econômica global e como ela se relaciona com políticas econômicas anteriores.
 Você não deve responder como se estivesse em uma conversa, mas sim como se estivesse escrevendo um artigo ou um blog.
 
 Os dados que você deve considerar são os seguintes:
-{df.to_string(index=False)}
+{df['conteudo'].to_string(index=False)}
+Há algumas notícias que não falam sobre tarifas, mas sim sobre outros assuntos. Você deve ignorar essas notícias e focar apenas nas que falam sobre tarifas do "Liberation Day".
 """
-st.subheader("Análise geral")
-st.write("Clique no botão abaixo para gerar uma análise geral sobre o impacto das tarifas no comércio internacional e na economia global.")
 if st.button("Gerar análise geral"):
     with st.spinner("Gerando resposta..."):
         response = generate_response(prompt_general)
         st.write(response)
 
-
+# ANALISE POR PAIS ==================================================================================================
 st.subheader("Análise por país")
 st.write("Selecione um país na lista baixo para gerar uma análise detalhada sobre o impacto das tarifas nesse país.")
-country = st.selectbox("País", ['Brasil', 'Estados Unidos', 'China'])
+country = st.selectbox("País", ['Brasil', 'Estados Unidos', 'China', 'Canadá'])
 
 prompt_country = f"""
 Você é um economista e está analisando notícias relacionadas as tarifas aplicadas pelo presidente Donald Trump no dia chamado por ele de "Liberation Day".
@@ -128,10 +80,10 @@ Destaque de forma breve quais motivos te levaram a atribuir esse score.
 Aponte também quais setores da economia podem ser mais afetados e como isso pode impactar a economia do país {country} a curto e longo prazo.
 Você não deve responder como se estivesse em uma conversa, mas sim como se estivesse escrevendo um artigo ou um blog.
 
-Os dados que você deve considerar são os seguintes:
-{df.to_string(index=False)}
+Os dados que você deve considerar são os seguintes. Foque apenas nesses dados. Os jornais utilizados são Agencia Brasil, Brasil de Fato e BCB do Canadá.:
+{df['conteudo'].to_string(index=False)}
+Há algumas notícias que não falam sobre tarifas, mas sim sobre outros assuntos. Você deve ignorar essas notícias e focar apenas nas que falam sobre tarifas do "Liberation Day".
 """
-
 st.subheader(f"Análise para o {country}")
 if st.button(f"Gerar análise para o {country}"):
     with st.spinner("Gerando resposta..."):
@@ -139,28 +91,68 @@ if st.button(f"Gerar análise para o {country}"):
         st.write(response)
 
 ## ANALISE POR JORNAL ==================================================================================================
+st.subheader("Análise por jornal")
+st.write("Clique no botão abaixo para entender como cada jornal está reportando o evento.")
 
+prompt_journal = f"""
+Você é um economista e está analisando notícias relacionadas as tarifas aplicadas pelo presidente Donald Trump no dia chamado por ele de "Liberation Day".
+Por favor, responda em português.
+Foque em como cada jornal está reportando os possíveis impactos no comércio internacional e na economia global.
+Destaque as diferenças entre os jornais e tente fazer uma análise comparativa entre eles.
+Se possível, identifique possíveis vieses ou tendências nas reportagens de cada jornal. Mas por favor, não invente nada.
+Você não deve responder como se estivesse em uma conversa, mas sim como se estivesse escrevendo um artigo ou um blog.
 
-# Adicionando um mapa interativo com Folium
+Os dados que você deve considerar são os seguintes:
+{df[['url', 'titulo']].to_string(index=False)}
+"""
+
+if st.button("Gerar análise por jornal"):
+    with st.spinner("Gerando resposta..."):
+        response = generate_response(prompt_journal)
+        st.write(response)
+
+# MAPA INTERATIVO ==================================================================================================
+st.subheader("Mapa interativo")
+st.write("Nesta seção, criamos um mapa interativo utilizando a biblioteca Folium. O mapa mostra os países afetados pelas tarifas aplicadas pelo presidente Donald Trump no dia chamado por ele de 'Liberation Day'.")
+st.write("O mapa é interativo e permite que você visualize os países afetados de forma dinâmica. Você pode dar zoom e explorar diferentes regiões do mundo.")
+
 # Criação do mapa com Folium
-m = folium.Map(location=[-23.55052, -46.633308], zoom_start=12)  # São Paulo
+# importando os dados de tarifas por país que esta na pasta auxiliares
+tarifa_por_pais = pd.read_excel("tarifas_por_pais.xlsx")
+m = folium.Map(
+location=[-50, 20],
+zoom_start=2.4,
+tiles="cartodb positron",
+max_bounds=True,
+max_bounds_style={"color": "black", "weight": 2, "fillOpacity": 0.5},
 
-# # Adiciona um marcador
-# folium.Marker(
-#     [-23.55052, -46.633308],
-#     popup='Centro de SP',
-#     tooltip='Clique para ver'
-# ).add_to(m)
+prefer_canvas=True,
+dragging=True)
 
+folium.Choropleth(
+    geo_data=requests.get('https://raw.githubusercontent.com/python-visualization/folium/main/examples/data/world-countries.json').json(),
+    data=tarifa_por_pais,
+    columns=['Country', 'Tarifa'],
+    nan_fill_color="black",
+nan_fill_opacity=0.1,
+key_on="feature.properties.name",
+fill_color="RdBu_r",
+legend_name="Tarifa (%)").add_to(m)
+
+        
 # Renderiza como HTML e mostra no Streamlit
 map_html = m._repr_html_()
-st.markdown("### Mapa interativo com Folium")
-html(map_html, height=500)
+html(map_html, height=800)
 
 
 # Perguntas frequentes
 st.subheader("Perguntas frequentes")
-st.write("Selecionamos alguns tópicos que são importantes para entender o nosso projeto. Selecione uma opção da lista abaixo para entender melhor algum tópico que ficar em dúvida.")
+st.markdown("""
+Nesta seção, utilizamos a API do Gemini para responder explicar alguns conceitos ou tecnologias que utilizamos no projeto.
+Aproveite para aprender sobre como funciona alguma biblioteca ou alguma ferramenta utilizada no projeto.
+
+Selecione um tópico na lista abaixo e clique no botão para gerar uma resposta.
+""")
 
 topico = st.selectbox("Tópico", ['Web Scraping', 'Streamlit', 'Folium', 'SQL', 'Gemini', 'API', 'Pandas', 'Python', 'Github', 'Análise de Sentimentos'])
 
